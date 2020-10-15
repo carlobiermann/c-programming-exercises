ca4c8613e2f6d677280adab68bdfaefbcd1915fa
carlobiermann <carlo.biermann@protonmail.com>
2020-10-16 01:07:25 +0200

    handle invalid inputs

--- 09_table_game/array_scan.c
+++ 09_table_game/array_scan.c
@@ -1,15 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define SSIZE (4)
+char field[SSIZE]; /* 'R','C','\0', '\n' */
+
 int main() 
-{
-	char field[3]={'_','X','\n'};
+{ 
+	int inputSize;
 
-	printf("%c \n", field[0]);
-
-	scanf("%s", field);
-
-	printf("Index [1]: %c Index [0]: %c \n", field[1], field[0]);
-
+	while((fgets(field, SSIZE,stdin)) != NULL ){
+		if(sizeof(field) != 4){
+			printf("Invalid Input, try again \n");		
+			continue;
+		} else {	
+		printf("Index [1]: %c Index [0]: %c \n", field[1], field[0]);
+		break;
+		}
+	}
 	return 0;
 }
